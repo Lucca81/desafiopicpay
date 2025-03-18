e948671cb7ff718dd10ee24ed74ecfd618dd31c5
Lucca Nascimento <luccadev@hotmail.com>
2025-03-18 12:12:31 -0300

    feat: create UserRepository

---
+++ src/main/java/com/desafiopicpay/desafiobackendpicpay/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.desafiopicpay.desafiobackendpicpay.repositories;
+
+import com.desafiopicpay.desafiobackendpicpay.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByDocument (String document);
+    Optional<User> findUserById (Long id);
+}
